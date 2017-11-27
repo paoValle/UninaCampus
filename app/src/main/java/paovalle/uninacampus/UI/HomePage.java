5fce00bae8bf4800c6ebb09e019f12e452614f8e
Paolo Valletta <paovalletta@hotmail.it>
2017-11-27 18:41:34 +0100

    Up App Android #2

--- app/src/main/java/paovalle/uninacampus/UI/HomePage.java
+++ app/src/main/java/paovalle/uninacampus/UI/HomePage.java
@@ -1,42 +1,32 @@
 package paovalle.uninacampus.UI;
 
-import android.net.Uri;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import paovalle.uninacampus.R;
 
-public class HomePage extends AppCompatActivity implements MenuGraf.OnFragmentInteractionListener {
-
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+
         Button menuBtn = (Button) findViewById(R.id.menuBtn);
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LinearLayout ll = (LinearLayout) findViewById(R.id.linearfrag);
-                ll.setVisibility(View.VISIBLE);
-                Log.w("INFO", "CLICCATO");
-
+                ((LinearLayout)findViewById(R.id.fragcontainer)).setVisibility(View.VISIBLE);
+                ((LinearLayout)findViewById(R.id.fragcontainer)).bringToFront();
             }
         });
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
 
     }
+
 }
