dc89ba8a3e3a53c43edc0240f35429ebb738137a
Paolo Valletta <paovalletta@hotmail.it>
2017-11-30 12:07:35 +0100

    Fix elenco corsi seguiti

--- app/src/main/java/paovalle/uninacampus/UI/HomePage.java
+++ app/src/main/java/paovalle/uninacampus/UI/HomePage.java
@@ -17,6 +17,7 @@ import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -39,6 +40,12 @@ public class HomePage extends AppCompatActivity {
     private NavigationView nvDrawer;
     private ActionBarDrawerToggle drawerToggle;
     private UtenteRegistrato user;
+    //la usero per ricavare l'id del corso selezionato
+    LinkedList<String> idCorsiSeguiti = new LinkedList<>();
+    int item_selected = -1;
+
+    private Button recBtn;
+    private Button mailBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,20 +59,6 @@ public class HomePage extends AppCompatActivity {
         ((TextView)findViewById(R.id.textLName)).setText(user.getCognome());
         ((TextView)findViewById(R.id.textMean)).setText(new Double(user.getMedia()).toString());
         ((TextView)findViewById(R.id.textCDL)).setText(user.getCorso().getNome());
-
-        //mostro elenco corsi seguiti
-        ListView lv = (ListView) findViewById(R.id.elencoCorsiSeguiti);
-
-        List<String> corsi = new LinkedList<>();
-        Iterator it = user.getCorsiScelti().entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            corsi.add(((Corso) pair.getValue()).getNome());
-            it.remove(); // avoids a ConcurrentModificationException
-        }
-
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, R.layout.row,corsi);
-        lv.setAdapter(adapter);
 
         // Set a Toolbar to replace the ActionBar.
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -89,7 +82,52 @@ public class HomePage extends AppCompatActivity {
         //if not null, this will turn menu icon to grey
         ((NavigationView)findViewById(R.id.nvView)).setItemIconTintList(null);
 
+        recBtn = (Button) findViewById(R.id.recorderBtn);
+        recBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToRecorder();
+            }
+        });
 
+        mailBtn = (Button) findViewById(R.id.mailBtn);
+        mailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMail();
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //mostro elenco corsi seguiti
+        ListView lv = (ListView) findViewById(R.id.elencoCorsiSeguiti);
+        lv.setAdapter(null);
+        List<String> corsi = new LinkedList<>();
+        Iterator it = user.getCorsiScelti().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            corsi.add(((Corso) pair.getValue()).getNome());
+            idCorsiSeguiti.add((String)pair.getKey());
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        System.out.println("RESUMED!");
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, R.layout.row,corsi);
+        lv.setAdapter(adapter);
+
+        //aggiungo listener on item selected
+        lv.setClickable(true);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                item_selected = i;
+                System.out.println(i);
+            }
+        });
     }
 
 
@@ -125,7 +163,12 @@ public class HomePage extends AppCompatActivity {
 
                 break;
             case R.id.nav_fifth_fragment:
+<<<<<<< refs/remotes/origin/master
                 attemptSendMail();
+=======
+                Log.d("prova","Sto clickando msg" );
+                goToMail();
+>>>>>>> Fix elenco corsi seguiti
                 break;
             case R.id.nav_sixth_fragment:
                 Log.d("prova","HocliccatoLibrettooooo" );
@@ -144,11 +187,24 @@ public class HomePage extends AppCompatActivity {
         mDrawer.closeDrawers();
     }
 
-    private void attemptSendMail(){
+    private void goToMail(){
+        ///prendo corso selezionato
+        String sel = (String)((ListView)findViewById(R.id.elencoCorsiSeguiti)).getSelectedItem();
+        System.out.println(sel);
+        if (item_selected==-1) {
+            Toast.makeText(getApplicationContext(), "Selezionare prima un corso!", Toast.LENGTH_SHORT).show();
+        } else {
+            System.out.println("Hai cliccato su: " + idCorsiSeguiti.get(item_selected));
+            Intent intent = new Intent(this, MailProf.class);
+            startActivity(intent);
+        }
+
+    }
+
+    private void goToRecorder(){
         //TODO: PAOLO DEVE FARE LA LOGICA!!!!
-        Log.d("prova","Sono nella funzione" );
-        Intent intent = new Intent(this,MailProf.class);
-        startActivity(intent);
+        /*Intent intent = new Intent(this,RecorderActivity.class);
+        startActivity(intent);*/
 
     }
 
